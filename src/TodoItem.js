e76eca2a9269b99974594aee75e7053b2fb8b49b
townechow <townechow@gmail.com>
2019-03-10 20:00:17 +0800

    代码优化
    代码拆分、this指向绑定写法优化、数据修改符合ES6语法，添加注释。

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -1,20 +1,33 @@
 import React, {Component, Fragment} from 'react';
 
 class TodoItem extends Component {
-    constructor(props){
+    constructor(props) {
         super(props);
         this.handleClick = this.handleClick.bind(this); // 绑定this指向，这样写节约性能
     }
+
     render() {
+        const content = this.props.content; {/**接收父组件传过来的数据item */}
+        
         return (
             <Fragment>
-               <div onClick= {this.handleClick}>
-               {this.props.content}</div> {/**接收父组件传过来的数据 */}
+               <li
+               onClick= {this.handleClick}
+               >
+               {content}
+               </li> 
+               {console.log('返回li')}
             </Fragment>
         )
     }
+    
+    // 方法：点击事件，接受父组件的删除方法和参数。
     handleClick() {
-        this.props.deleteItem(this.props.index) // 同时接收了父组件传过来的deleteItem方法和index数据
+        const deleteItem = this.props.deleteItem;
+        const index = this.props.index;  // 同时接收了父组件传过来的deleteItem方法和index数据
+        deleteItem(index); 
+        // this.props.deleteItem(this.props.index)
+        console.log('删除方法')
 
     }
 }
--- src/TodoList.js
+++ src/TodoList.js
@@ -1,6 +1,6 @@
 import React, { Component, Fragment} from 'react';
-import './style.css' 
 import TodoItem from './TodoItem.js'
+import './style.css' 
 
 class TodoList extends Component {
     constructor(props) {  //构造函数
@@ -9,7 +9,12 @@ class TodoList extends Component {
             inputValue:'',
             list: []
         }
+
+        this.handleBtnClick = this.handleBtnClick.bind(this);
+        this.handleInputChange = this.handleInputChange.bind(this);
+        this.handleItemDelete = this.handleItemDelete.bind(this);  
     }
+    
     render() {
         return (
             <Fragment>
@@ -19,46 +24,72 @@ class TodoList extends Component {
                     id="insertArea" 
                     className = 'input'
                     value = {this.state.inputValue} //与状态中的数据绑定
-                    onChange={this.handleInputChange.bind(this)} // 事件绑定==>监听改变，执行相应方法，方法需定义,绑定this指向。
+                    onChange={this.handleInputChange} // 事件绑定==>监听改变，执行相应方法，方法需定义,绑定this指向。
                     /> 
-                    <button onClick={this.handleBtnClick.bind(this)}>提交</button>
+                    <button onClick={this.handleBtnClick}>提交</button>
                 </div>
                 <ul>
-                  {this.state.list.map((item,index) => {
-                      return (
-                          <TodoItem 
-                          content={item} 
-                          index={index} 
-                          deleteItem={this.handleItemDelete.bind(this)}
-                          /> // 将数据和方法传给子组件。
-                    ) 
-                  })
-                    }
+                  { this.getTodoItem() }
                 </ul>
             </Fragment>
         )
     }
+    
+    // 
+    getTodoItem(){
+       return this.state.list.map((item,index) => {
+            return (
+                <TodoItem 
+                key = {index} 
+                content={item} 
+                index={index} 
+                deleteItem={this.handleItemDelete}
+                />
+                // 将数据和方法传给子组件。
+          ) 
+        })
+    }
+
+    // 方法：监听改变，将改变的值传入数据inputValue.
     handleInputChange(e) {
     //    console.log(e.target.value)  //获取输入框的值
-        this.setState({
-          inputValue:e.target.value //用setState()方法将获取到的值传给组件 => 改变组件数据
-      })
+        const value = e.target.value;
+        this.setState(() =>({
+            inputValue: value
+        })); 
+    //     this.setState({
+    //       inputValue:e.target.value //用setState()方法将获取到的值传给组件 => 改变组件数据
+    //   })
+        {console.log('键盘输入')}
     }
+
+    //方法：点击事件，将输入框的值-inputValue传入数据list，并清空输入框。
     handleBtnClick(){
-        this.setState({
-            list:[...this.state.list, this.state.inputValue], //参数一展开原有项，参数二将输入值增加到list.
-            inputValue: ''  //将输入框清空。
-        })
+        this.setState((prevState) => ({
+            list: [...prevState.list, prevState.inputValue],
+            inputValue: ''
+        }));
+        // this.setState({
+        //     list:[...this.state.list, this.state.inputValue], //参数一展开原有项，参数二将输入值增加到list.
+        //     inputValue: ''  //将输入框清空。
+        // })
 
     }
+    
+    //方法：点击事件，将list 数据做一份拷贝，删除副本下标为index的项，一项，将改变了的list 返回数据。
     handleItemDelete(index) {
-        // console.log(index);
-        const list = [...this.state.list]; //把数据list做一份拷贝。
-        list.splice(index, 1); // 删除下标为index的内容，一个。
+        this.setState((prevState) => {
+            const list = [...prevState.list];
+            list.splice(index, 1);
+            return {list} 
+        });
+        // // console.log(index);
+        // const list = [...this.state.list]; //把数据list做一份拷贝。
+        // list.splice(index, 1); // 删除下标为index的内容，一个。
 
-        this.setState({
-            list:list
-        }) //将改变了的list传回状态数据
+        // this.setState({
+        //     list:list
+        // }) //将改变了的list传回状态数据
 
 
     }
