273e5044ebbc68d959676318929fffe1a8004881
townechow <townechow@gmail.com>
2019-04-01 15:59:36 +0800

    无状态组件
    只有render函数时使用无状态组件具有更高性能

--- src/TodoListUI.js
+++ src/TodoListUI.js
@@ -3,36 +3,65 @@
 import React, {Component} from 'react';
 import  {Input,Button,List} from 'antd';
 
-class TodoListUI extends Component {
-    render() {
-        return(
-            <div>
-                <div style={{marginLeft:'20px'}}>
-                <Input value={this.props.inputValue} 
-                placeholder="re info" 
-                style={{width:'300px',marginTop:'20px'}} 
-                onChange={this.props.handleInputChange}
-                />
-                <Button 
-                style={{marginLeft:'15px'}}
-                onClick={this.props.handleBtnClick}
-                >提交
-                </Button>
-                <List
-                style={{marginTop:'20px',width:'300px'}}
-                bordered
-                dataSource={this.props.list}
-                renderItem={(item,index) => (
-                <List.Item 
-                // onClick={this.state.handleItemDelet.bind(this,index)}
-                onClick={(index) => {this.props.handleItemDelet(index)}}
-                >{item}
-                </List.Item>)}
-                />
-                </div>
-            </div>
-        )
-    }
-} 
+const TodoListUI = (props) => {
+    return (<div>
+        <div style={{marginLeft:'20px'}}>
+        <Input value={props.inputValue} 
+        placeholder="re info" 
+        style={{width:'300px',marginTop:'20px'}} 
+        onChange={props.handleInputChange}
+        />
+        <Button 
+        style={{marginLeft:'15px'}}
+        onClick={props.handleBtnClick}
+        >提交
+        </Button>
+        <List
+        style={{marginTop:'20px',width:'300px'}}
+        bordered
+        dataSource={props.list}
+        renderItem={(item,index) => (
+        <List.Item 
+        // onClick={this.state.handleItemDelet.bind(this,index)}
+        onClick={(index) => {props.handleItemDelet(index)}}
+        >{item}
+        </List.Item>)}
+        />
+        </div>
+    </div>
+
+    )
+}
+// class TodoListUI extends Component {
+//     render() {
+//         return(
+//             <div>
+//                 <div style={{marginLeft:'20px'}}>
+//                 <Input value={this.props.inputValue} 
+//                 placeholder="re info" 
+//                 style={{width:'300px',marginTop:'20px'}} 
+//                 onChange={this.props.handleInputChange}
+//                 />
+//                 <Button 
+//                 style={{marginLeft:'15px'}}
+//                 onClick={this.props.handleBtnClick}
+//                 >提交
+//                 </Button>
+//                 <List
+//                 style={{marginTop:'20px',width:'300px'}}
+//                 bordered
+//                 dataSource={this.props.list}
+//                 renderItem={(item,index) => (
+//                 <List.Item 
+//                 // onClick={this.state.handleItemDelet.bind(this,index)}
+//                 onClick={(index) => {this.props.handleItemDelet(index)}}
+//                 >{item}
+//                 </List.Item>)}
+//                 />
+//                 </div>
+//             </div>
+//         )
+//     }
+// } 
 
 export default TodoListUI;
